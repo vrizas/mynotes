6a9485eb3c100406c927da4a9b1eba0be14f32a8
vrizas <vrizasizza@gmail.com>
2022-07-28 10:11:46 +0700

    create header bar

---
+++ src/components/NavigationList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function NavigationList() {
+ return (
+    <nav>
+        <button>
+            <span className="material-icons icon">inventory</span>
+        </button>
+        <button>
+            <span className="material-icons icon">add_box</span>
+        </button>
+    </nav>
+ );
+}
+ 
+export default NavigationList;
---
+++ src/components/NotesApp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeaderBar from './HeaderBar';
+ 
+class ContactApp extends React.Component {
+  render() {
+    return (
+      <div id="app">
+        <HeaderBar />
+      </div>
+    );
+  }
+}
+ 
+export default ContactApp;

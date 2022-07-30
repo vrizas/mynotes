c4bcca861d7ca1bc849b86bc9b26faab27e9e8c0
vrizas <vrizasizza@gmail.com>
2022-07-30 13:13:35 +0700

    implement header responsive

--- src/components/NavigationList.js
+++ src/components/NavigationList.js
@@ -5,9 +5,11 @@ function NavigationList() {
     <nav>
         <button>
             <span className="material-icons icon">inventory</span>
+            <span className="text">Arsip</span>
         </button>
         <button>
             <span className="material-icons icon">add_box</span>
+            <span className="text">Tambah Catatan</span>
         </button>
     </nav>
  );

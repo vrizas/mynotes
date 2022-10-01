b26b326f01fb784c6e7ccf0f41741d2e956f99f1
vrizas <vrizasizza@gmail.com>
2022-10-01 11:28:24 +0700

    implement reusable component to note list

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import AddNoteModal from './AddNoteModal';
-import ArchivedNoteList from './ArchivedNoteList';
 import NoteList from './NoteList';
 import SearchInput from './SearchInput';
 
@@ -9,8 +8,8 @@ function MainContent({ notes, addNoteHandler, deleteNoteHandler, archiveNoteHand
    <main>
       <SearchInput notes={notes} searchNoteHandler={searchNoteHandler} />
       <AddNoteModal addNoteHandler={addNoteHandler} />
-      <NoteList notes={notes} deleteNoteHandler={deleteNoteHandler} archiveNoteHandler={archiveNoteHandler} />
-      <ArchivedNoteList notes={notes} deleteNoteHandler={deleteNoteHandler} archiveNoteHandler={archiveNoteHandler} />
+      <NoteList notes={notes} deleteNoteHandler={deleteNoteHandler} archiveNoteHandler={archiveNoteHandler} isArchived={false} />
+      <NoteList notes={notes} deleteNoteHandler={deleteNoteHandler} archiveNoteHandler={archiveNoteHandler} isArchived={true} />
    </main>
  );
 }
--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,22 +1,41 @@
 import React from 'react';
 import NoteItem from './NoteItem';
  
-function NoteList({ notes, deleteNoteHandler, archiveNoteHandler }) {
-  const filteredNotes = notes.filter(note => !note.archived);
+function NoteList({ notes, deleteNoteHandler, archiveNoteHandler, isArchived }) {
+  if (isArchived) {
+    const filteredNotes = notes.filter(note => note.archived);
 
-  return (
-    <div className="note-list">
-      <div className="note-list__content">
-        {
-          (filteredNotes.length > 0) ? 
-          filteredNotes.map(note => (
-            <NoteItem key={note.id} {...note} deleteNoteHandler={deleteNoteHandler} archiveNoteHandler={archiveNoteHandler} />
-          )) : 
-          <p>Tidak ada catatan</p>
-        }
+    return (
+      <div className="archived-note-list" id="archived-note-list">
+          <h2 className="archived-note-list__title">Catatan yang telah diarsipkan</h2>
+          <div className="archived-note-list__content">
+            {
+              (filteredNotes.length > 0) ? 
+                filteredNotes.map(note => (
+                  <NoteItem key={note.id} {...note} deleteNoteHandler={deleteNoteHandler} archiveNoteHandler={archiveNoteHandler} />
+                )) : 
+                <p>Tidak ada catatan yang diarsipkan</p>
+            }
+          </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const filteredNotes = notes.filter(note => !note.archived);
+
+    return (
+      <div className="note-list">
+        <div className="note-list__content">
+          {
+            (filteredNotes.length > 0) ? 
+            filteredNotes.map(note => (
+              <NoteItem key={note.id} {...note} deleteNoteHandler={deleteNoteHandler} archiveNoteHandler={archiveNoteHandler} />
+            )) : 
+            <p>Tidak ada catatan</p>
+          }
+        </div>
+      </div>
+    );
+  }
 }
  
 export default NoteList;

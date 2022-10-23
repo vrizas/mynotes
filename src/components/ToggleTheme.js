bab4dea381cf74ba842536bfa89571fe4ea08469
Vrizas Izza <vrizas.magang.sekawanmedia@gmail.com>
2022-10-23 12:13:38 +0700

    adjust toggle theme component to use hooks

--- src/components/ToggleTheme.js
+++ src/components/ToggleTheme.js
@@ -1,13 +1,10 @@
-import { ThemeConsumer } from '../contexts/ThemeContext';
+import React from 'react';
+import ThemeContext from '../contexts/ThemeContext';
  
 function ToggleTheme() {
-  return (
-    <ThemeConsumer>
-      {({ theme, toggleTheme }) => {
-        return <button onClick={toggleTheme} className="toggle-theme-button">{theme === 'light' ?  <span className="material-icons icon icon-light">light_mode</span>: <span className="material-icons icon icon-dark">dark_mode</span>}</button>;
-      }}
-    </ThemeConsumer>
-  );
+  const { theme, toggleTheme } = React.useContext(ThemeContext);
+
+  return <button onClick={toggleTheme} className="toggle-theme-button">{theme === 'light' ?  <span className="material-icons icon icon-light">light_mode</span>: <span className="material-icons icon icon-dark">dark_mode</span>}</button>
 }
  
 export default ToggleTheme;
